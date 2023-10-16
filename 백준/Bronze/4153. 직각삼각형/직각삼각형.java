b03bae54d53cb6b1ac7df3fd6519138927be910a
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2023-10-16 22:20:33 +0900

    [Bronze III] Title: 직각삼각형, Time: 204 ms, Memory: 17492 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[3];
+        while(true){
+            for(int i = 0; i < 3; i++){
+                nums[i] = sc.nextInt();
+            }
+            if(nums[0] == 0 && nums[1] == 0 && nums[2] == 0){
+                break;
+            }
+            Arrays.sort(nums);
+            if(Math.pow(nums[2],2) == (Math.pow(nums[0],2) + Math.pow(nums[1],2))){
+                System.out.println("right");
+            }
+            else{
+                System.out.println("wrong");
+            }
+        }
+
+
+    }
+}
