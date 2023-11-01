f161726668c19c396e8c522810cabdd1b32f50a9
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2023-11-01 14:26:50 +0900

    [Bronze IV] Title: 특별한 날, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == 2 && b == 18){
+            System.out.println("Special");
+        }
+        else if(a == 1 || (a == 2 && b < 18)){
+            System.out.println("Before");
+        }
+        else if(a > 2 || (a == 2 && b > 18)){
+            System.out.println("After");
+        }
+
+    }
+}
