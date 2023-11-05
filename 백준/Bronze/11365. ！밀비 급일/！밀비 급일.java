6b7427329165e9ab13748f896b694a7d6234b7f5
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2023-11-05 23:57:33 +0900

    [Bronze IV] Title: !밀비 급일, Time: 232 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String a = sc.nextLine();
+            if(a.equals("END")){
+                break;
+            }
+            for(int i = a.length()-1; i>=0; i--){
+                System.out.print(a.charAt(i));
+            }
+            System.out.print("\n");
+
+
+        }
+    }
+}
