48e0010e2d3ccafe49993d2c8858756802a61e2c
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2024-01-24 14:58:29 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int[] height = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; st.hasMoreTokens(); i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(height);
+
+        for (int i = 0; i < n; i++) {
+            if(height[i] <= l){
+                l++;
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(l);
+
+    }
+}
