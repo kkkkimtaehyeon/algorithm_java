89abead9aa31837636a39234844e567d6944dd9c
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2024-09-03 14:59:37 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
