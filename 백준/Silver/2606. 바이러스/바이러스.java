aae3f8aa49ec8c0dafa73402fb396fffec0419d1
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2025-11-09 15:12:47 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
+    static int[][] graph;
     static boolean[] visited;
-    static int answer = -1;
+    static int[] dx = new int[]{1, 0, -1, 0};
+    static int[] dy = new int[]{0, 1, 0, -1};
+    static int v;
+    static int e;
+    static int count = -1;
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
 
-        int computer = sc.nextInt();
-        int related = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        e = Integer.parseInt(st.nextToken());
 
-        arr = new int[computer+1][computer+1];
-        visited = new boolean[computer+1];
+        visited = new boolean[v + 1];
+        graph = new int[v + 1][v + 1];
 
-        for(int i = 0; i < related; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
-            arr[a][b] = arr[b][a] = 1;
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = graph[b][a] = 1;
         }
         dfs(1);
-        System.out.println(answer);
-
+        System.out.println(count);
     }
-    public static void dfs(int start) {
-        visited[start] = true;
-        answer++;
 
-        for(int i = 0 ; i < arr.length; i++) {
-            if(arr[start][i] == 1 && visited[i] == false) {
+    static void dfs(int v) {
+        count++;
+        visited[v] = true;
+
+        for (int i = 1; i < graph[v].length; i++) {
+            if (graph[v][i] == 1 && !visited[i]) {
                 dfs(i);
             }
         }
     }
-
-
 }
