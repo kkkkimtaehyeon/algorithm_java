95126194dfd5658c97ccdc8fdb131aedf159f3f0
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2025-12-07 15:45:40 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
 SELECT date_format(datetime, '%H') as hour, count(*) as count
 from animal_outs
-where date_format(datetime, '%H:%i') between '09:00' and '19:59' 
-group by date_format(datetime, '%H') 
+group by hour
+having hour >= 9 and hour < 20
 order by hour
