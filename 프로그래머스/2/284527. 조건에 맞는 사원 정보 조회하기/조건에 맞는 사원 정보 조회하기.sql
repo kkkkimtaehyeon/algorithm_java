1a12a853a6810b60b2cd5788f6822eab46c2521a
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2026-03-29 21:32:37 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,12 +1,16 @@
--- 코드를 작성해주세요
-# HR_DEPARTMENT -- 회사 부서 정보
-# HR_EMPLOYEES -- 회사 사원 정보
-# HR_GRADE -- 22년 사원 평가 정보
+# select sum(hg.score) as score, he.emp_no, he.emp_name, he.position, he.email
+# from HR_EMPLOYEES he
+# join HR_GRADE hg on hg.emp_no = he.emp_no
+# group by he.emp_no, he.emp_name, he.position, he.email
+# order by score desc
+# limit 1;
 
-select sum(hg.score) as score, he.emp_no, he.emp_name, he.position, he.email
+select hg.score, he.emp_no, he.emp_name, he.position, he.email
 from HR_EMPLOYEES he
-join HR_GRADE hg on hg.emp_no = he.emp_no
-join HR_DEPARTMENT hd on hd.dept_id = he.dept_id
-group by he.emp_no
-order by score desc
-limit 1;
+inner join (
+    select sum(score) as score, emp_no
+    from HR_GRADE
+    group by emp_no
+    order by score desc
+    limit 1
+    ) hg on hg.emp_no = he.emp_no;
